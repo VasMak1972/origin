cffc02d53b0b78935aad12cd5d54576cc5985c7c
Vasiliy <vasvmz@mail.ru>
2024-07-21 18:28:08 +0300

    add Projects

---
+++ Projects/CMakeProject1/out/build/x64-Debug/libpqxx-build/test/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Projects/libpqxx/libpqxx-7.9.1/test
+# Build directory: C:/Projects/CMakeProject1/out/build/x64-Debug/libpqxx-build/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(runner "C:/Projects/CMakeProject1/out/build/x64-Debug/libpqxx-build/test/runner.exe")
+set_tests_properties(runner PROPERTIES  _BACKTRACE_TRIPLES "C:/Projects/libpqxx/libpqxx-7.9.1/test/CMakeLists.txt;12;add_test;C:/Projects/libpqxx/libpqxx-7.9.1/test/CMakeLists.txt;0;")
